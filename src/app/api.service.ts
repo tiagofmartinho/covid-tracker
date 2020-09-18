47cce5a33a9b91eac3d06e001bd576f7acceea12
Tiago Martinho <tfmss1@iscte-iul.pt>
2020-09-18 19:18:53 +0100

    News updates

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,31 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+require("dotenv").config();
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
-
   constructor(private http: HttpClient) {}
 
   public getCases() {
-    return this.http.get('https://covid-19-data.p.rapidapi.com/country/code?format=json&code=pt', {
-      observe: 'body',
-      responseType: 'json',
-      headers: {'x-rapid-host': 'covid-19-data.append.rapidapi.com',
-      'x-rapidapi-key': '786e7fb420msh598b079080031acp1aa33fjsna43b5092e70a'
-    }
-    }).toPromise();
+    return this.http
+      .get(
+        "https://covid-19-data.p.rapidapi.com/country/code?format=json&code=pt",
+        {
+          observe: "body",
+          responseType: "json",
+          headers: {
+            "x-rapid-host": "covid-19-data.append.rapidapi.com",
+            "x-rapidapi-key": process.env.RAPID_API_KEY,
+          },
+        }
+      )
+      .toPromise();
   }
 
   public getNews() {
-    return this.http.get('https://newsapi.org/v2/top-headlines?' +
-    'q=covid' +
-    '&pagesize=3' +
-    '&country=pt', {
-      observe: 'body',
-      responseType: 'json',
-      headers: {'X-Api-Key': 'dc3b65c2307242b299efd1f6350e4cba'} // this shouldn't be exposed but security isn't an issue in this case
-    }).toPromise();
+    return this.http
+      .get(
+        "https://newsapi.org/v2/top-headlines?" +
+          "q=covid" +
+          "&pagesize=3" +
+          "&country=pt",
+        {
+          observe: "body",
+          responseType: "json",
+          headers: { "X-Api-Key": "dc3b65c2307242b299efd1f6350e4cba" }, // this shouldn't be exposed but security isn't an issue in this case
+        }
+      )
+      .toPromise();
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "./api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent  {
-  title = 'covid-tracker';
+export class AppComponent {
+  title = "covid-tracker";
 
   current;
   stats;
   news;
 
-  constructor(private apiService: ApiService){
-     this.apiService.getCases().then((data => {
+  constructor(private apiService: ApiService) {
+    this.apiService.getCases().then((data) => {
       this.stats = data[0];
       console.log(this.stats);
-      this.current = this.stats.confirmed - this.stats.recovered - this.stats.deaths;
-    }));
-  }
+      this.current =
+        this.stats.confirmed - this.stats.recovered - this.stats.deaths;
+    });
 
+    this.apiService.getNews().then((news) => {
+      this.news = news;
+      console.log(this.news);
+    });
+  }
 }
