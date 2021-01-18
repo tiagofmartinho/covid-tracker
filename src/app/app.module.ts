6619e94cd4466dc030929e3ccc1811c6a5fa1d2e
Tiago Martinho <tiago.martinho@criticaltechworks.com>
2021-01-18 11:38:03 +0000

    change local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { LOCALE_ID, NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HttpClientModule } from "@angular/common/http";
-import { DatePipe } from "@angular/common";
+import { DatePipe, registerLocaleData } from "@angular/common";
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [NgbModule, BrowserModule, HttpClientModule],
-  providers: [DatePipe],
+  providers: [DatePipe,   { provide: LOCALE_ID, useValue: 'pt-PT'},],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
